e575a5c550b79152517e149fede4ed6c04fa3ca9
Vishwa1234 <126213911+VishwaJaiganesh@users.noreply.github.com>
2024-08-05 15:54:01 +1000

    Finished Geography and History Qs

---
+++ Trivia_Game_1.py
@@ -0,0 +1,106 @@
+# Define categories or subjects that user can play
+categories=["Geography","History","Science","Sports","Video Games","Music","TV & Movies","quit","geography","history","science","sports","video games","Music","tv & movies","Quit"]
+# Define difficulty levels that user can play
+difficulty=["Very Easy","Easy","Normal", "Medium", "Hard","Very Hard",'Extreme',"very easy","easy","normal", "medium", "hard","very hard",'extreme']
+ 
+ 
+# Create a list of questions with their corresponding category, difficulty, answer and score
+questions = [
+    {
+        "category": "Geography",
+        "difficulty": "Very Easy",
+        "question": "What continent is Japan on?",
+        "answer": "Asia",
+        "score":100
+    },
+    {
+        "category": "Geography",
+        "difficulty": "Easy",
+        "question": "What is the tallest mountain?",
+        "answer": "Mt.Everest",
+         "score":250
+    },
+    {
+        "category": "Geography",
+        "difficulty": "Normal",
+        "question": "Capital of Brazil?",
+        "answer": "Brasilia",
+         "score":500
+    },
+    {
+        "category": "Geography",
+        "difficulty": "Medium",
+        "question": "What is the name of the World’s Largest Desert?",
+        "answer": "Antartica",
+        "score":750
+    },
+    {
+        "category": "Geography",
+        "difficulty": "Hard",
+        "question": "What body of water is located west of Saudi Arabia?",
+        "answer": "The Red Sea",
+        "score":1000
+   },
+    {
+        "category": "Geography",
+        "difficulty": "Very Hard",
+        "question": "What native language is spoken in Scotland?",
+        "answer": "Scotland Gaelic",
+         "score":1250
+    },
+    {
+        "category": "Geography",
+        "difficulty": "Extreme",
+        "question": "How many countries and territories have the Union Jack in their flags?",
+        "answer": "23",
+         "score":1500
+    },
+     {
+        "category": "History",
+        "difficulty": "Very Easy",
+        "question": "Who won the space race?",
+        "answer": "USSR",
+         "score":100
+    },
+    {
+        "category": "History",
+        "difficulty": "Easy",
+        "question": "What was the term for the Spanish colonists?",
+        "answer": "Conquistador",
+         "score":250
+    },
+    {
+        "category": "History",
+        "difficulty": "Normal",
+        "question": "Who was the ruler of the Mongol Empire?",
+        "answer": "Genghis Khan",
+        "score":500
+    },
+    {
+        "category": "History",
+        "difficulty": "Medium",
+        "question": "What year was the battle of Hastings?",
+        "answer": "1066",
+         "score":750
+    },
+    {
+        "category": "History",
+        "difficulty": "Hard",
+        "question": "Who died causing WW1?",
+        "answer": "Franz Ferdinand",
+        "score":1000
+    },
+    {
+        "category": "History",
+        "difficulty": "Very Hard",
+        "question": "Who was the world’s richest person?",
+        "answer": "Mansa Musa",
+         "score":1250
+    },
+    {
+        "category": "History",
+        "difficulty": "Extreme",
+        "question": "What years were the start and end of the Bubonic Plague?",
+        "answer": "1346 to 1353",
+         "score":1500
+    }]
